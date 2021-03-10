58b75e5fe72853191728553d689dee2553455d43
Douglas Cardoso <douglascf12@live.com>
2021-03-09 23:31:15 -0300

    repository funcionario criada

---
+++ src/main/java/com/douglascf/lojavirtual/repositories/FuncionarioRepository.java
@@ -0,0 +1,10 @@
+package com.douglascf.lojavirtual.repositories;
+
+import com.douglascf.lojavirtual.models.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
+    
+    
+    
+}
