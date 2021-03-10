df9bfb921c5b05e19fceb6db01f0b6327d7989e6
Douglas Cardoso <douglascf12@live.com>
2021-03-09 22:27:30 -0300

    banco de dados configurado

---
+++ src/main/java/com/douglascf/lojavirtual/DataConfiguration.java
@@ -0,0 +1,35 @@
+package com.douglascf.lojavirtual;
+
+import javax.activation.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:8080/loja?useTimezone=true&serverTimezone=America/Sao_Pulo");
+        dataSource.setUsername("root");
+        dataSource.setUsername("");
+        return (DataSource) dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+    }
+
+}
